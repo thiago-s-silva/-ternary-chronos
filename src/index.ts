e9f88b92d08e9dd1c7b32c6045d0444d39a5dc5b
thiago.silva <thiago.sdd@live.com>
2023-11-07 14:05:49 -0300

    chore: add compodoc for documentation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export default function () {
+/**
+ * @description This is a sample function
+ * @returns {void}
+ * @example
+ * import helloWorld from './index';
+ * helloWorld(); // Hello World
+ */
+export default function printHello () {
   console.log("Hello World");
 }
