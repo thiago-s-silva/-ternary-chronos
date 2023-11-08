a942f0405c49a132c24ae2f348eec578e7e9af5d
thiago.silva <thiago.sdd@live.com>
2023-11-08 11:23:16 -0300

    chore: add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-/**
- * @description This is a sample function
- * @returns {void}
- * @example
- * import helloWorld from './index';
- * helloWorld(); // Hello World
- */
-export default function printHello () {
-  console.log("Hello World");
-}
+const temp = 'alou'
